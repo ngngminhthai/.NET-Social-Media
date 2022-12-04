e2b136521e732f6231cb3cd6acb8654699c7a711
nguyenminhthaijs <thainguyenminh441@gmail.com>
2022-12-04 12:59:34 +0700

    Add mui

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
 
 export default function NavBar() {
-    const {activityStore} = useStore();
 
     return (
         <Menu inverted fixed='top'>
@@ -14,7 +12,6 @@ export default function NavBar() {
                 </Menu.Item>
                 <Menu.Item name='Activities' />
                 <Menu.Item>
-                    <Button onClick={() => activityStore.openForm()} positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,12 +4,9 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
-  <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>,
+    <App />,
   document.getElementById('root')
 );
 
